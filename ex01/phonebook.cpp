76ed05f98a2c4cd8eae3e75adf156747a3984ab2
ysoroko <ysoroko@student.s19.be>
2021-09-14 14:39:05 +0200

    ex01 started, Makefile, header and main created

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 14:18:41 by ysoroko           #+#    #+#             */
+/*   Updated: 2021/09/14 14:37:05 by ysoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+static void	ft_add(void)
+{
+	return ;
+}
+
+static void	ft_search(void)
+{
+	return ;
+}
+
+int main(void)
+{
+	Contact			contact;
+	std::string		user_command;
+
+	while (1)
+	{
+		std::cout << "Please enter the command (SEARCH / ADD / EXIT)\n>";
+		std::cin >> user_command;
+		if (user_command == "EXIT")
+			return (0);
+		else if (user_command == "ADD")
+			ft_add();
+		else if (user_command == "SEARCH")
+			ft_search();
+		else
+			std::cout << "Only ADD, SEARCH and EXIT commands are accepted\n";
+	}
+	return (0);
+}
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 14:13:06 by ysoroko           #+#    #+#             */
+/*   Updated: 2021/09/14 14:18:09 by ysoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+
+class Contact
+{
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
+public:
+	Contact(/* args */);
+	~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+#endif
